0d0994b89562a64306b5759049c6a7c1a6b8b1d5
Answer901 <1941016403@qq.com>
2022-04-21 23:18:00 +0800

    04211

---
+++ 04211/04211/test.c
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int score = 0;
+	while (scanf("%d", &score) != EOF)
+	{
+		if (score >= 90 && score <= 100)
+			printf("Perfect");
+	}
+	return 0;
+}
