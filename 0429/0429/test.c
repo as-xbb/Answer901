d192730d0a619f7a97f9e2283a35057ccc4d2894
赛 安 <1941016403@qq.com>
2022-04-29 22:13:47 +0800

    0429

---
+++ 0429/0429/test.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//static修饰函数
+
+//声明来自外部的函数
+extern int Add(int, int);
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int sum = Add(a, b);
+	printf("%d\n", sum);
+
+	return 0;
+}
