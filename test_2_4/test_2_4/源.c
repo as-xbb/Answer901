56d433dd721df1701aac21db92edca0e15b57191
answer901 <1941016403@qq.com>
2023-02-04 18:52:16 +0800

    指针

---
+++ test_2_4/test_2_4/源.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int* test()
+//{
+//	int a = 10;
+//	printf("%d\n", a);
+//	return &a;
+//}
+//int main()
+//{
+//	int* p = test();
+//	*p = 100;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	//				0 1 2 3 4 5 6 7 8  9
+//	int* p = arr;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", * (p + i));
+//	}
+//	/*printf("%p\n", arr);
+//	printf("%p\n", &arr[0]);*/
+//	/*printf("%d\n", sizeof(arr));*/
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%p\n", arr);
+//	printf("%p\n", arr+1);
+//
+//	printf("%p\n", &arr[0]);
+//	printf("%p\n", &arr[0]+1);
+//
+//	printf("%p\n", &arr);
+//	printf("%p\n", &arr+1);
+//
+//	return 0;
+//}
+
