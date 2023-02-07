fa8c79e4368ead2ad0d48698dfe7aea2aebf8daa
answer901 <1941016403@qq.com>
2023-02-07 23:50:35 +0800

    指针数组

---
+++ test_2_7/test_2_7/源.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;//1
+//	*pc = 'a';
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	const char* p = "abcdef";
+//
+//	//*p = 'w';//err
+//
+//	printf("%c\n", *p);
+//	printf("%s\n", p);
+//
+//	//
+//	//p是指针变量，在x86环境下是4个字节
+//	//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    char str1[] = "hello bit.";
+//    char str2[] = "hello bit.";
+//    const char* str3 = "hello bit.";
+//    const char* str4 = "hello bit.";
+//    if (str1 == str2)
+//        printf("str1 and str2 are same\n");
+//    else
+//        printf("str1 and str2 are not same\n");
+//
+//    if (str3 == str4)
+//        printf("str3 and str4 are same\n");
+//    else
+//        printf("str3 and str4 are not same\n");
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//
+//	int* p1 = &a;
+//	int* p2 = &b;
+//	int* p3 = &c;
+//
+//	int* arr[3] = { &a,&b,&c };//arr就是一个指针数组
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d", *(arr[i]));
+//	}
+//
+//	return 0;
+//}
+
+
